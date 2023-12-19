114744f64cdc45d153e2dacc5fd7a4aa98417c26
Bruno Carnavalli <bruno.carnavalli@compasso.com.br>
2023-12-19 20:24:21 -0300

    feat: add Zod properties

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,67 @@
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { useState } from 'react'
 import './styles/global.css'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const createUserFormSchema = z.object({
+  email: z.string()
+    .min(1, 'Email is required')
+    .email('Invalid email format'),
+  password: z.string()
+    .min(1, 'Password is required')
+    .min(6, 'Password must be at least 6 characters'),
+})
+
+type CreateUserFormData = z.infer<typeof createUserFormSchema>
+
 
 function App() {
+  const [output, setOutput] = useState<string>('')
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<CreateUserFormData>({
+    resolver: zodResolver(createUserFormSchema)
+  })
+
+  const createUser = (data: any) => {
+    setOutput(JSON.stringify(data, null, 2))
+  }
+
   return (
     <>
-      <main className='h-screen bg-zinc-950 text-zinc-300 flex flex-col items-center justify-center gap-[10vh]'>
+      <main className='h-screen bg-zinc-950 text-zinc-300 flex flex-col items-center justify-center gap-[5vh]'>
 
-        <h1 className='text-2xl text-center'>Registration Form</h1>
-
-        <form className='flex flex-col items-center w-full max-w-xs h-[40vh] gap-10'>
-          <div className='flex flex-col w-full'>
-            <label htmlFor="name">Name</label>
-            <input
-              type="text"
-              className='border-zinc-200 shadow-sm rounded px-3 h-10'
-            />
-          </div>
+        <form
+          onSubmit={handleSubmit(createUser)}
+          className='flex flex-col items-center w-full max-w-xs h-[40vh] gap-10'
+        >
           <div className='flex flex-col w-full'>
             <label htmlFor="email">Email</label>
             <input
-              type="email"
-              className='border-zinc-200 shadow-sm rounded px-3 h-10'
+              className='border-zinc-600 bg-zinc-900 shadow-sm rounded px-3 h-10'
+              {...register('email')}
             />
+            {errors.email && <span>{errors.email.message}</span>}
           </div>
 
           <div className='flex flex-col w-full'>
             <label htmlFor="password">Password</label>
             <input
               type="password"
-              className='border-zinc-200 shadow-sm rounded px-3 h-10'
+              className='border-zinc-600 bg-zinc-900 shadow-sm rounded px-3 h-10'
+              {...register('password')}
             />
+            {errors.password && <span>{errors.password.message}</span>}
           </div>
 
           <button type="submit" className='w-full h-10 rounded font-semibold text-white bg-emerald-500 mt-5'>Save</button>
         </form>
+
+        <pre>{output}</pre>
       </main>
     </>
   )
