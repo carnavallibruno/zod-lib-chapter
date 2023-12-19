8c0a30c3c38358814b0c3e7d04bed0ace29e70f9
Bruno Carnavalli <bruno.carnavalli@compasso.com.br>
2023-12-19 18:44:12 -0300

    feat: Created base layout for form page

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './styles/global.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className='h-screen bg-zinc-950 text-zinc-300 flex flex-col items-center justify-center gap-[10vh]'>
+
+        <h1 className='text-2xl text-center'>Registration Form</h1>
+
+        <form className='flex flex-col items-center w-full max-w-xs h-[40vh] gap-10'>
+          <div className='flex flex-col w-full'>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              className='border-zinc-200 shadow-sm rounded px-3 h-10'
+            />
+          </div>
+          <div className='flex flex-col w-full'>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              className='border-zinc-200 shadow-sm rounded px-3 h-10'
+            />
+          </div>
+
+          <div className='flex flex-col w-full'>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className='border-zinc-200 shadow-sm rounded px-3 h-10'
+            />
+          </div>
+
+          <button type="submit" className='w-full h-10 rounded font-semibold text-white bg-emerald-500 mt-5'>Save</button>
+        </form>
+      </main>
     </>
   )
 }
