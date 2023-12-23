eda4ecd2f973ac0f3f3f68439f718023c324d5d9
Bruno Carnavalli <bruno.carnavalli@compasso.com.br>
2023-12-23 18:32:07 -0300

    feat: add field array field and validations

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
+import { IoMdClose } from "react-icons/io"
 import { useFormSchema } from './hook/useFormSchema'
 import './styles/global.css'
-import Input from './components/Input'
 
 function App() {
   const [output, setOutput] = useState<string>('')
@@ -13,8 +13,23 @@ function App() {
   const {
     register,
     handleSubmit,
-    errors
+    errors,
+    fields,
+    append,
+    remove
   } = useFormSchema()
+
+  function addGame() {
+    append({ name: '', skill: '--' })
+  }
+
+  const gameSkillsOptions = [
+    '--',
+    'None',
+    'Noob',
+    'Normal',
+    'Hardcore'
+  ]
 
   return (
     <>
@@ -22,21 +37,79 @@ function App() {
 
         <form
           onSubmit={handleSubmit(createUser)}
-          className='flex flex-col items-center w-full max-w-xs h-[40vh] gap-10'
+          className='flex flex-col items-center w-full max-w-xs gap-10'
         >
-          <Input
-            label='Email'
-            type='email'
-            error={errors.email?.message}
-            register={{...register('email')}}
-          />
+          <div className='flex flex-col w-full'>
+            <label htmlFor="email">Email</label>
+            <input
+              type='text'
+              className='border-zinc-600 bg-zinc-900 shadow-sm rounded px-3 h-10'
+              {...register('email')}
+            />
+            {errors.email && <span className="text-xs text-red-600">{errors.email?.message}</span>}
+          </div>
 
-          <Input
-            label='Password'
-            type='password'
-            error={errors.password?.message}
-            register={{...register('password')}}
-          />
+          <div className='flex flex-col w-full'>
+            <label htmlFor="email">Password</label>
+            <input
+              type='password'
+              className='border-zinc-600 bg-zinc-900 shadow-sm rounded px-3 h-10'
+              {...register('password')}
+            />
+            {errors.password && <span className="text-xs text-red-600">{errors.password?.message}</span>}
+          </div>
+
+          <div className='w-full flex flex-col gap-1'>
+            <div className='flex justify-between items-center'>
+              <label htmlFor=''>Games</label>
+              <button
+                className='text-emerald-500 text-sm'
+                type='button'
+                onClick={addGame}
+              >
+                Add
+              </button>
+            </div>
+
+            {fields.map((field, index) => {
+              return (
+                <div key={field.id} className='w-full flex justify-between items-center gap-2'>
+                  <div className='flex flex-col gap-1 w-[60%]'>
+                    <input
+                      type='text'
+                      className='border-zinc-600 bg-zinc-900 shadow-sm rounded px-3 h-10'
+                      {...register(`games.${index}.name`)}
+                    />
+
+                    {errors.games?.[index]?.name && <span className="text-xs text-red-600">{errors.games?.[index]?.name?.message}</span>}
+                  </div>
+
+                  <div className='flex flex-col gap-1'>
+                    <select
+                      className='border-zinc-600 bg-zinc-900 shadow-sm rounded px-3 h-10'
+                      {...register(`games.${index}.skill`)}
+                    >
+                      {gameSkillsOptions.map((skill, index) => (
+                        <option key={index} value={skill}>{skill}</option>
+                      ))}
+                    </select>
+                    {errors.games?.[index]?.skill && <span className="text-xs text-red-600">{errors.games?.[index]?.skill?.message}</span>}
+                  </div>
+                  {fields.length > 1 &&
+                    <button
+                      className={(errors.games?.[index]?.name || errors.games?.[index]?.skill) && 'mb-5'}
+                      type='button'
+                      onClick={() => remove(index)}>
+                      <IoMdClose
+                        color="red"
+                      />
+                    </button>
+                  }
+                </div>
+              )
+            })}
+          </div>
+
 
           <button type="submit" className='w-full h-10 rounded font-semibold text-white bg-emerald-500 mt-5'>Save</button>
         </form>
--- src/hook/useFormSchema.ts
+++ src/hook/useFormSchema.ts
@@ -1,16 +1,28 @@
 import React from 'react'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useForm } from 'react-hook-form'
+import { useFieldArray, useForm } from 'react-hook-form'
 import { z } from 'zod'
 
 export const useFormSchema = () => {
   const createUserFormSchema = z.object({
     email: z.string()
       .min(1, 'Email is required')
-      .email('Invalid email format'),
+      .email('Invalid email format')
+      .refine(value => {
+        return value.endsWith('@compasso.com.br')
+      }, 'E-mail address must be from Compass')
+    ,
     password: z.string()
       .min(1, 'Password is required')
       .min(6, 'Password must be at least 6 characters'),
+    games: z.array(z.object({
+      name: z.string().min(1, 'Game name is required'),
+      skill: z.enum(['--', 'None', 'Noob', 'Normal', 'Hardcore'])
+        .refine(value => {
+          return value !== '--'
+        }
+          , 'Skill level is required')
+    }))
   })
 
   type CreateUserFormData = z.infer<typeof createUserFormSchema>
@@ -19,12 +31,28 @@ export const useFormSchema = () => {
     resolver: zodResolver(createUserFormSchema)
   })
 
-  const { register, handleSubmit, formState: { errors } } = createUserForm
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    control
+  } = createUserForm
 
+  const {
+    fields,
+    append,
+    remove
+  } = useFieldArray({
+    control,
+    name: 'games'
+  })
 
   return {
     register,
     handleSubmit,
-    errors
+    errors,
+    fields,
+    append,
+    remove
   }
 }
